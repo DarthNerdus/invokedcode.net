b96d2504c34d2f21057baeec6c84adcd62bea4b8
Jesse Read <jread@invokedcode.net>
2013-09-24 14:53:19 -0400

    Pass at new site.

---
+++ next/resources/script.js
@@ -0,0 +1,16 @@
+/* Author:
+	Paul Bremer 2012 (@paul_bremer)
+*/
+
+$('.carousel').carousel({
+  interval: 20000
+})
+
+/* Screenshots w/ PrettyPhoto */
+
+$("a[rel^='prettyPhoto']").prettyPhoto({
+	show_title: false,
+	theme: 'pp_default',
+	social_tools: false
+});
+
