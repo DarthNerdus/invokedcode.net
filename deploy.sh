2864ad2e2ceb641a9b5b116997da0a2562aa330e
Jesse Read <jread@invokedcode.net>
2013-10-07 23:38:36 -0400

    Stop deleting everything on deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-s3cmd sync --acl-public --delete-removed --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ s3://invokedcode.net/
+s3cmd sync --acl-public --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ s3://invokedcode.net/
