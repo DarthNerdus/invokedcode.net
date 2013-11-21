f1336bcff697866d0f86a079bc6312b63e259f3a
Jesse Read <jread@invokedcode.net>
2013-11-21 08:47:22 -0500

    Deploy script for vps

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-s3cmd sync --acl-public --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ s3://invokedcode.net/
+#s3cmd sync --acl-public --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ s3://invokedcode.net/
+rsync -avzP --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ do-ic-1:/var/www/invokedcode
