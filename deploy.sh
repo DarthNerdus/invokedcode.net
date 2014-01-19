57a4ccf9d0ba97db40c8250f4c72f4b00ce95513
Jesse Read <jread@invokedcode.net>
2014-01-19 14:19:39 -0500

    Exclude templates folder when deploying.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #s3cmd sync --acl-public --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ s3://invokedcode.net/
-rsync -avzP --exclude '*.sh' --exclude '.git/*' --exclude '*.psd' --exclude '.DS_Store' ./ do-ic-1:/var/www/invokedcode
+rsync -avzP --exclude '*.sh' --exclude '.git/*' --exclude 'templates' --exclude '*.psd' --exclude '.DS_Store' ./ do-ic-1:/var/www/invokedcode
